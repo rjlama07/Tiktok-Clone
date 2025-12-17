af1b840abde694c3a13a8cef96af79a45152eaad
Rj lama <riteshlama5@gmail.com>
2025-12-17 14:25:45 +1100

    Initial Commit

---
+++ Tiktok Clone/Tiktok_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tiktok_CloneApp.swift
+//  Tiktok Clone
+//
+//  Created by Ritesh jung lama on 17/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Tiktok_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
